ee6fe8d10c825e1ec2aab8fb2af313b818a9eacb
Manuel Jiménez <man.jime@gmail.com>
2017-06-05 21:27:00 -0500

    Mas funcionalidaddes contra la SHD

--- src/main/java/co/com/assist/leasing/LeasingQuery.java
+++ src/main/java/co/com/assist/leasing/LeasingQuery.java
@@ -7,8 +7,11 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -25,6 +28,7 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 
 import co.com.assist.leasing.objects.Comparendo;
 import co.com.assist.leasing.objects.Identificacion;
@@ -45,6 +49,8 @@ public class LeasingQuery {
 	// query.consultarPagos("UUT352");
 	// }
 
+	private DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
 	@WebMethod
 	@WebResult(name = "vehiculo")
 	public Vehiculo conultarVehiculoPorVIN(@WebParam(name = "vin") String vin) {
@@ -60,12 +66,111 @@ public class LeasingQuery {
 
 	@WebMethod
 	@WebResult(name = "impuesto")
-	public Impuesto[] conultarObligacionesPendientes(@WebParam(name = "placa") String placa,
-			@WebParam(name = "entidad") String entidad) {
-		return new Impuesto[0];
+	public Impuesto conultarValorVigencia(@WebParam(name = "placa") String placa,
+			@WebParam(name = "vigencia") int vigencia) {
+		String uri = "http://recursosweb.shd.gov.co/ServiciosSitII/resources/predial-ac-648/obtenerFormulario?vigencia="
+				+ vigencia + "&periodo=0&impuesto=3&objeto=" + placa;
+
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		WebResource service = client.resource(UriBuilder.fromUri(uri).build());
+		String json = service.accept(MediaType.APPLICATION_JSON).get(String.class);
+
+		// System.out.println(json);
+
+		JsonParser jsonParser = new JsonParser();
+		JsonObject jo = (JsonObject) jsonParser.parse(json);
+
+		JsonArray datos = jo.get("datos").getAsJsonArray();
+		Iterator<JsonElement> datosIterator = datos.iterator();
+
+		int idSoporte = 0;
+		while (datosIterator.hasNext()) {
+			JsonObject dato = datosIterator.next().getAsJsonObject();
+
+			if (dato.get("cod").getAsString().equals("idSoporte")) {
+				idSoporte = dato.get("desc").getAsInt();
+				break;
+			}
+		}
+
+		uri = "http://recursosweb.shd.gov.co/Tareaps/TaxPayment.jsp";
+		service = client.resource(UriBuilder.fromUri(uri).build());
+		MultivaluedMapImpl values = new MultivaluedMapImpl();
+		values.add("idSoporte", idSoporte + "");
+		values.add("valor", "");
+
+		String html = service.accept(MediaType.APPLICATION_FORM_URLENCODED).post(String.class, values);
+		int start = html.indexOf("<input type=\"hidden\" id=\"refer\" value=\"") + 39;
+		int end = html.indexOf("\"", start);
+
+		long id = Long.parseLong(html.substring(start, end));
+
+		uri = "http://recursosweb.shd.gov.co/Tareaps/resources/presentacion/" + id;
+		service = client.resource(UriBuilder.fromUri(uri).build());
+		json = service.accept(MediaType.APPLICATION_JSON).get(String.class);
+
+		jo = (JsonObject) jsonParser.parse(json);
+
+		Impuesto impuesto = new Impuesto();
+		impuesto.setVigencia(vigencia + "");
+		try {
+			if (jo.get("fechaVcto") != null) {
+				impuesto.setFechaDeVencimiento(dateFormat.parse(jo.get("fechaVcto").getAsString()));
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		impuesto.setSoporte(jo.get("rutaFormulario").getAsString() + idSoporte);
+		impuesto.setValorPagar(jo.get("valorPagar").getAsBigDecimal());
+		impuesto.setEntidad(jo.get("mensaje").getAsString());
+
+		return impuesto;
 	}
 
-	private DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+	@WebMethod
+	@WebResult(name = "impuesto")
+	public Impuesto[] conultarObligacionesPendientes(@WebParam(name = "placa") String placa) {
+		String uri = "http://recursosweb.shd.gov.co/ConsultaPagos/resources/servicios/relacionPagos?impuesto=3&objeto="
+				+ placa;
+
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		WebResource service = client.resource(UriBuilder.fromUri(uri).build());
+		String json = service.accept(MediaType.APPLICATION_JSON).get(String.class);
+
+		JsonParser jsonParser = new JsonParser();
+		JsonObject jo = (JsonObject) jsonParser.parse(json);
+
+		System.out.println(json);
+		
+		Set<Integer> vigenciasPendientes = new LinkedHashSet<>();
+		int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+		for (int year = Math.max(jo.get("dato2").getAsInt(), 2011); year <= currentYear; year++) {
+			vigenciasPendientes.add(year);
+		}
+
+		JsonArray detalles = jo.get("detalles").getAsJsonArray();
+		Iterator<JsonElement> detallesIterator = detalles.iterator();
+
+		while (detallesIterator.hasNext()) {
+			JsonObject detalle = detallesIterator.next().getAsJsonObject();
+
+			// System.out.println(detalle.get("vigencia").getAsInt());
+
+			vigenciasPendientes.remove(detalle.get("vigencia").getAsInt());
+		}
+
+		int i = 0;
+		Impuesto[] impuestos = new Impuesto[vigenciasPendientes.size()];
+		for (Integer vigenciaPendiente : vigenciasPendientes) {
+			Impuesto impuesto = new Impuesto();
+			impuesto.setVigencia(vigenciaPendiente + "");
+			impuestos[i++] = impuesto;
+		}
+
+		return impuestos;
+	}
 
 	@WebMethod
 	@WebResult(name = "impuesto")
--- src/main/java/co/com/assist/leasing/objects/Impuesto.java
+++ src/main/java/co/com/assist/leasing/objects/Impuesto.java
@@ -1,5 +1,6 @@
 package co.com.assist.leasing.objects;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 import javax.xml.bind.annotation.XmlElement;
@@ -9,12 +10,16 @@ import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 @XmlRootElement(name = "Impuesto")
-@XmlType(propOrder = { "vigencia", "nroTransaccion", "fechaDePago", "entidad" })
+@XmlType(propOrder = { "vigencia", "nroTransaccion", "fechaDePago", "fechaDeVencimiento", "valorPagar", "soporte",
+		"entidad" })
 public class Impuesto {
 
 	private String vigencia;
 	private String nroTransaccion;
 	private Date fechaDePago;
+	private Date fechaDeVencimiento;
+	private BigDecimal valorPagar;
+	private String soporte;
 	private String entidad;
 
 	public String getVigencia() {
@@ -42,6 +47,33 @@ public class Impuesto {
 
 	public void setFechaDePago(Date fechaDePago) {
 		this.fechaDePago = fechaDePago;
+	}
+
+	@XmlElement
+	@XmlSchemaType(name = "date")
+	@XmlJavaTypeAdapter(XmlDateAdapter.class)
+	public Date getFechaDeVencimiento() {
+		return fechaDeVencimiento;
+	}
+
+	public void setFechaDeVencimiento(Date fechaDeVencimiento) {
+		this.fechaDeVencimiento = fechaDeVencimiento;
+	}
+
+	public String getSoporte() {
+		return soporte;
+	}
+
+	public void setSoporte(String soporte) {
+		this.soporte = soporte;
+	}
+
+	public BigDecimal getValorPagar() {
+		return valorPagar;
+	}
+
+	public void setValorPagar(BigDecimal valorPagar) {
+		this.valorPagar = valorPagar;
 	}
 
 	public String getEntidad() {
