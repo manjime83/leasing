048909f1d78d01675f8ee8764c737b0efe8ec7b1
Manuel Jiménez <man.jime@gmail.com>
2017-07-24 09:16:40 -0500

    intregracion RUNT

--- src/main/java/co/com/assist/leasing/LeasingQuery.java
+++ src/main/java/co/com/assist/leasing/LeasingQuery.java
@@ -1,8 +1,13 @@
 package co.com.assist.leasing;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.math.BigDecimal;
+import java.net.URI;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -11,24 +16,32 @@ import java.util.Calendar;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.core.util.MultivaluedMapImpl;
 
 import co.com.assist.leasing.objects.Comparendo;
 import co.com.assist.leasing.objects.Identificacion;
@@ -43,24 +56,131 @@ import servicios.Exception_Exception;
 @WebService
 public class LeasingQuery {
 
-	// public static void main(String[] args) {
-	// LeasingQuery query = new LeasingQuery();
-	//
-	// query.consultarPagos("UUT352");
-	// }
+	public static void main(String[] args) throws Exception {
+
+		LeasingQuery query = new LeasingQuery();
+
+		Identificacion id = new Identificacion();
+		id.setTipo("");
+		id.setNumero("80777377");
+
+		query.conultarVehiculoPorPlaca(id, "JYR29D");
+
+	}
 
 	private DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
-	@WebMethod
-	@WebResult(name = "vehiculo")
-	public Vehiculo conultarVehiculoPorVIN(@WebParam(name = "vin") String vin) {
-		return new Vehiculo();
+	private Client client = IgnoreSSLClient();
+
+	public static Client IgnoreSSLClient() {
+		// System.setProperty("http.proxyHost", "127.0.0.1");
+		// System.setProperty("http.proxyPort", "8888");
+		// System.setProperty("https.proxyHost", "127.0.0.1");
+		// System.setProperty("https.proxyPort", "8888");
+		//
+		// try {
+		// SSLContext sslcontext = SSLContext.getInstance("TLS");
+		// sslcontext.init(null, new TrustManager[] { new X509TrustManager() {
+		// public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+		// }
+		//
+		// public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+		// }
+		//
+		// public X509Certificate[] getAcceptedIssuers() {
+		// return new X509Certificate[0];
+		// }
+		// } }, new java.security.SecureRandom());
+		//
+		// ClientConfig clientConfig = new ClientConfig();
+		//
+		// ClientBuilder builder = ClientBuilder.newBuilder().sslContext(sslcontext).withConfig(clientConfig);
+		//
+		// Map<String, Object> props = builder.getConfiguration().getProperties();
+		// for (Entry<String, Object> prop : props.entrySet()) {
+		// System.out.println(prop.getKey() + "," + prop.getValue());
+		// }
+		//
+		// return builder.build();
+		// } catch (Exception e) {
+		// return null;
+		// }
+		return ClientBuilder.newClient();
+	}
+
+	public LeasingQuery() {
+
 	}
 
 	@WebMethod
 	@WebResult(name = "vehiculo")
 	public Vehiculo conultarVehiculoPorPlaca(@WebParam(name = "identificacion") Identificacion id,
 			@WebParam(name = "placa") String placa) {
+		URI runt = UriBuilder.fromUri("https://www.runt.com.co/consultaCiudadana").build();
+
+		WebTarget target = client.target(runt).path("captcha");
+		Response response = target.request("image/webp", "image/apng", "image/*", "*/*;q=0.8")
+				.header("Referer", "https://www.runt.com.co/consultaCiudadana/").acceptEncoding("gzip", "deflate", "br")
+				.acceptLanguage("en-US", "en;q=0.8", "es-US;q=0.6", "es;q=0.4").get();
+		Map<String, NewCookie> cookies = response.getCookies();
+
+		String captcha = "";
+		try {
+			InputStream stream = response.readEntity(InputStream.class);
+			byte[] bytes = IOUtils.toByteArray(stream);
+			File captchaFile = new File("captcha.jpg");
+			FileUtils.writeByteArrayToFile(captchaFile, bytes);
+			Desktop.getDesktop().open(captchaFile);
+			System.out.print("captcha: ");
+			Scanner sc = new Scanner(System.in);
+			captcha = sc.nextLine();
+			sc.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		target = client.target(runt).path("publico/automotores");
+		Invocation.Builder builder = target
+				.request(MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN, MediaType.WILDCARD)
+				.header("Referer", "https://www.runt.com.co/consultaCiudadana/").acceptEncoding("gzip", "deflate", "br")
+				.acceptLanguage("en-US", "en;q=0.8", "es-US;q=0.6", "es;q=0.4");
+		builder.header("X-XSRF-TOKEN", cookies.get("XSRF-TOKEN").getValue());
+		builder.header("PLACA_CONSULTA", placa);
+		builder.header("CEDULA_CONSULTA", id.getNumero());
+
+		for (NewCookie cookie : cookies.values()) {
+			builder.cookie(cookie);
+		}
+
+		JsonObject obj = new JsonObject();
+		obj.addProperty("tipoDocumento", "C");
+		obj.addProperty("procedencia", "NACIONAL");
+		obj.addProperty("tipoConsulta", "1");
+		obj.add("vin", null);
+		obj.addProperty("noDocumento", "80777377");
+		obj.addProperty("noPlaca", placa);
+		obj.add("soat", null);
+		obj.add("codigoSoat", null);
+		obj.addProperty("captcha", captcha);
+
+		String json = builder.post(Entity.entity(obj.toString(), MediaType.APPLICATION_JSON + ";charset=UTF-8"),
+				String.class);
+		System.out.println(json);
+
+		target = client.target(runt).path("publico/automotores/soats");
+		builder = target.request(MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN, MediaType.WILDCARD)
+				.header("Referer", "https://www.runt.com.co/consultaCiudadana/").acceptEncoding("gzip", "deflate", "br")
+				.acceptLanguage("en-US", "en;q=0.8", "es-US;q=0.6", "es;q=0.4");
+		builder.header("X-XSRF-TOKEN", cookies.get("XSRF-TOKEN").getValue());
+		builder.header("PLACA_CONSULTA", placa);
+		builder.header("CEDULA_CONSULTA", id.getNumero());
+		for (NewCookie cookie : cookies.values()) {
+			builder.cookie(cookie);
+		}
+		response = builder.get();
+
+		System.out.println(response.readEntity(String.class));
+
 		return new Vehiculo();
 	}
 
@@ -71,10 +191,8 @@ public class LeasingQuery {
 		String uri = "http://recursosweb.shd.gov.co/ServiciosSitII/resources/predial-ac-648/obtenerFormulario?vigencia="
 				+ vigencia + "&periodo=0&impuesto=3&objeto=" + placa;
 
-		ClientConfig config = new DefaultClientConfig();
-		Client client = Client.create(config);
-		WebResource service = client.resource(UriBuilder.fromUri(uri).build());
-		String json = service.accept(MediaType.APPLICATION_JSON).get(String.class);
+		WebTarget service = client.target(UriBuilder.fromUri(uri).build());
+		String json = service.request(MediaType.APPLICATION_JSON).get(String.class);
 
 		// System.out.println(json);
 
@@ -95,20 +213,21 @@ public class LeasingQuery {
 		}
 
 		uri = "http://recursosweb.shd.gov.co/Tareaps/TaxPayment.jsp";
-		service = client.resource(UriBuilder.fromUri(uri).build());
-		MultivaluedMapImpl values = new MultivaluedMapImpl();
-		values.add("idSoporte", idSoporte + "");
-		values.add("valor", "");
+		service = client.target(UriBuilder.fromUri(uri).build());
+		Form values = new Form();
+		values.param("idSoporte", idSoporte + "");
+		values.param("valor", "");
 
-		String html = service.accept(MediaType.APPLICATION_FORM_URLENCODED).post(String.class, values);
+		String html = service.request(MediaType.APPLICATION_FORM_URLENCODED)
+				.post(Entity.entity(values, MediaType.APPLICATION_FORM_URLENCODED_TYPE), String.class);
 		int start = html.indexOf("<input type=\"hidden\" id=\"refer\" value=\"") + 39;
 		int end = html.indexOf("\"", start);
 
 		long id = Long.parseLong(html.substring(start, end));
 
 		uri = "http://recursosweb.shd.gov.co/Tareaps/resources/presentacion/" + id;
-		service = client.resource(UriBuilder.fromUri(uri).build());
-		json = service.accept(MediaType.APPLICATION_JSON).get(String.class);
+		service = client.target(UriBuilder.fromUri(uri).build());
+		json = service.request(MediaType.APPLICATION_JSON).get(String.class);
 
 		jo = (JsonObject) jsonParser.parse(json);
 
@@ -134,16 +253,18 @@ public class LeasingQuery {
 		String uri = "http://recursosweb.shd.gov.co/ConsultaPagos/resources/servicios/relacionPagos?impuesto=3&objeto="
 				+ placa;
 
-		ClientConfig config = new DefaultClientConfig();
-		Client client = Client.create(config);
-		WebResource service = client.resource(UriBuilder.fromUri(uri).build());
-		String json = service.accept(MediaType.APPLICATION_JSON).get(String.class);
+		WebTarget service = client.target(UriBuilder.fromUri(uri).build());
+		String json = service.request(MediaType.APPLICATION_JSON).get(String.class);
 
 		JsonParser jsonParser = new JsonParser();
 		JsonObject jo = (JsonObject) jsonParser.parse(json);
 
-		System.out.println(json);
-		
+		if (jo.size() == 0) {
+			throw new RuntimeException("Vehiculo no encontrado en SHD. Solo soportamos vehiculos registrados en Bogotá.");
+		}
+
+		// System.out.println(json);
+
 		Set<Integer> vigenciasPendientes = new LinkedHashSet<>();
 		int currentYear = Calendar.getInstance().get(Calendar.YEAR);
 		for (int year = Math.max(jo.get("dato2").getAsInt(), 2011); year <= currentYear; year++) {
@@ -178,10 +299,8 @@ public class LeasingQuery {
 		String uri = "http://recursosweb.shd.gov.co/ConsultaPagos/resources/servicios/relacionPagos?impuesto=3&objeto="
 				+ placa;
 
-		ClientConfig config = new DefaultClientConfig();
-		Client client = Client.create(config);
-		WebResource service = client.resource(UriBuilder.fromUri(uri).build());
-		String json = service.accept(MediaType.APPLICATION_JSON).get(String.class);
+		WebTarget service = client.target(UriBuilder.fromUri(uri).build());
+		String json = service.request(MediaType.APPLICATION_JSON).get(String.class);
 
 		// System.out.println(json);
 
@@ -231,6 +350,12 @@ public class LeasingQuery {
 				List<Comparendo> comparendos = new ArrayList<Comparendo>(datosComparendos.size());
 
 				for (ClsDatosComparendos datosComparendo : datosComparendos) {
+					String estado = datosComparendo.getEstadoComparendo();
+
+					// if (estado.equals("Pendiente") || estado.equals("Pendiente Curso")) {
+					// continue;
+					// }
+
 					Comparendo comparendo = new Comparendo();
 					comparendo.setNumeroComparendo(datosComparendo.getNumeroComparendo());
 					comparendo.setFechaComparendo(dateFormat.parse(datosComparendo.getFechaComparendo()));
--- src/main/java/co/com/assist/leasing/service/LeasingServiceV1.java
+++ src/main/java/co/com/assist/leasing/service/LeasingServiceV1.java
@@ -14,16 +14,6 @@ public class LeasingServiceV1 {
 	@WebMethod
 	@WebResult(name = "fahrenheit")
 	public BigDecimal CelsiusToFahrenheit(@WebParam(name = "celsius") BigDecimal celsius) {
-		if (celsius.compareTo(BigDecimal.valueOf(60)) > 0) {
-			throw new RuntimeException();
-		}
-
-		try {
-			Thread.sleep(celsius.longValue() * 1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
 		return celsius.multiply(BigDecimal.valueOf(9)).divide(BigDecimal.valueOf(5), 2, RoundingMode.HALF_EVEN)
 				.add(BigDecimal.valueOf(32));
 	}
